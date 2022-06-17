66670f86f1ae4a9da4932e9e6bd313f9e9750226
Marco <marco.hoev@gmail.com>
2022-06-17 02:43:28 +0200

    minmax

--- project1/exp_wrapper.py
+++ project1/exp_wrapper.py
@@ -2,19 +2,25 @@ import time
 import subprocess as sub
 import os
 
-exp_running = 'experiments_all.py'
+exp_running = 'project1/experiments.py'
+count = 0
 
 time.sleep(1)
 print('Hello this is the exp_wrapper!')
 
 while True:
-   process = sub.run(['pgrep', '-af', 'python3'], stdout=sub.PIPE, stderr=sub.PIPE)
-   time.sleep(10)
-   if exp_running in process.stdout.decode():
-       pass
-   else:
-       os.system('nohup python3 experiments_all.py &')
-       print('Restarted script.')
+    if count > 20:
+        print('Restarted 20 times, killing process to be safe.')
+        break
+    process = sub.run(['pgrep', '-af', 'python'], stdout=sub.PIPE, stderr=sub.PIPE)
+    time.sleep(10)
+    if exp_running in process.stdout.decode():
+        pass
+    else:
+        os.system('conda activate ts')
+        os.system(f'nohup python {exp_running} &')
+        print('Restarted script.')
+        count += 1
 
-   print('Experiments running. Sleeping for 60s.')
-   time.sleep(60)
+    print('Experiments running. Sleeping for 60s.')
+    time.sleep(60)
